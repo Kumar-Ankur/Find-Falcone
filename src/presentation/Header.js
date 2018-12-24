32eed04fbbf53692b2fc9c65eba9544b50b89e33
Kumar Ankur <akakankur81@gmail.com>
2018-12-25 00:01:35 +0530

    Add coachmark js for home page

--- src/presentation/Header.js
+++ src/presentation/Header.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import Logo from '../../img/Logo.png';
+import Logo from "../../img/Logo.png";
 
 class Header extends Component {
   render() {
     return (
       <header className="header">
         <div className="header__logo-box">
-          <img src={ Logo } alt="logo" className="header__logo" />
+          <img src={Logo} alt="logo" className="header__logo" />
         </div>
         <div className="header__text-box">
           <h1 className="text-primary">
