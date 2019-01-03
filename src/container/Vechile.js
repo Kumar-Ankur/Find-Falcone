db2f538c3070a324369d3377f49ce32b8262e290
sape_ankur <kumar.ankur@sapient.com>
2019-01-03 16:27:42 +0530

    Update radio box disable

--- src/container/Vechile.js
+++ src/container/Vechile.js
@@ -2,7 +2,6 @@ import React, { Component, Fragment } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import { RadioButton, RadioButtonGroup } from "material-ui/RadioButton";
 
-
 const styles = {
   block: {
     maxWidth: 250,
@@ -15,7 +14,6 @@ const styles = {
   }
 };
 
-
 class Vechile extends Component {
   constructor(props) {
     super(props);
@@ -23,7 +21,8 @@ class Vechile extends Component {
     this.state = {
       vechile_list: [],
       PlanetDetails: this.props.PlanetDetails,
-      vechile_name: ""
+      vechile_name: "",
+      selectedVechile: []
     };
   }
 
@@ -34,48 +33,63 @@ class Vechile extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    this.setState({ PlanetDetails: nextProps.PlanetDetails })
+    this.setState({ PlanetDetails: nextProps.PlanetDetails });
   }
 
   filteredData = (value, vechileDetails) => {
     let selectedVechile = vechileDetails.filter(checkedVechile => {
-      if(checkedVechile.name === value) {
+      if (checkedVechile.name === value) {
         return checkedVechile;
       }
-    })
+    });
     return selectedVechile;
-  }
+  };
 
-  vechileSelected = (value, PlanetDetails,vechileDetails) => {
-    let selectedVechile = this.filteredData(value, vechileDetails);
-    console.log(value, PlanetDetails, selectedVechile);
-    if(PlanetDetails.distance > selectedVechile.max_distance) {
-      this.setState({ vechile_name: value })
-    }
-  }
+  vechileSelected = (value, PlanetDetails, vechileDetails) => {
+    console.log(value, PlanetDetails, selectedVechile, vechileDetails);
+  };
 
   render() {
     const renderVechile = this.state.vechile_list.map(vech => {
-        return(
-            <RadioButton
-            value= {`${vech.name}`}
+      if (
+        this.state.PlanetDetails.distance >
+        vech.max_distance
+      ) {
+        return (
+          <RadioButton
+            value={`${vech.name}`}
             label={`${vech.name} (${vech.total_no})`}
             style={styles.radioButton}
-            disabled = {vech.name === this.state.vechile_name ? true : false }
+            disabled={true}
           />
-        )
-    })
+        );
+      } else {
+        return (
+          <RadioButton
+            value={`${vech.name}`}
+            label={`${vech.name} (${vech.total_no})`}
+            style={styles.radioButton}
+          />
+        );
+      }
+    });
 
     return (
       <div className="section-vechile">
         <MuiThemeProvider>
-        <RadioButtonGroup 
-        name="shipSpeed" 
-        defaultSelected="not_light"
-        onChange = {(event,value) => this.vechileSelected(value, this.state.PlanetDetails, this.state.vechile_list)}
-        >
-         { renderVechile }
-        </RadioButtonGroup>
+          <RadioButtonGroup
+            name="shipSpeed"
+            defaultSelected="not_light"
+            onChange={(event,value) =>
+              this.vechileSelected(
+                value,
+                this.state.PlanetDetails,
+                this.state.vechile_list
+              )
+            }
+          >
+            {renderVechile}
+          </RadioButtonGroup>
         </MuiThemeProvider>
       </div>
     );
