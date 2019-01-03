7428ca96e398e2a1964652b45f8e8628477dc573
unknown <anjalisingh538@gmail.com>
2019-01-03 22:46:01 +0530

    Update styling

--- src/presentation/FindFalcone.js
+++ src/presentation/FindFalcone.js
@@ -3,6 +3,7 @@ import React, { Component, Fragment } from "react";
 import Planet from "../container/Planet";
 import Navbar from "./Navbar";
 import Time from "../container/Time";
+import Footer from "./Footer";
 
 class FindFalcone extends Component {
   render() {
@@ -15,7 +16,9 @@ class FindFalcone extends Component {
           <a href="#" className="navbtn navbtn--animated navbtn--white">
             Ready to find??
           </a>
+          
         </section>
+        <Footer />
       </div>
     );
   }
