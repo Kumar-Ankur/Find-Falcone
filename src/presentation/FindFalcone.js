82e26e4e5a3954cb174cfc1302da33bd83e10165
Kumar Ankur <akakankur81@gmail.com>
2019-01-09 21:45:37 +0530

    first deployment

--- src/presentation/FindFalcone.js
+++ src/presentation/FindFalcone.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import Planet from "../container/Planet";
-import Navbar from "./Navbar";
-import Footer from "../container/Footer";
+import Planet from '../container/Planet';
+import Navbar from './Navbar';
+import Footer from '../container/Footer';
 
 class FindFalcone extends Component {
-  render() {
-    return (
-      <div className="section-falcone">
-        <Navbar />
-        <Planet />
-        <section className="section-btn" />
-        <Footer />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="section-falcone">
+				<Navbar />
+				<Planet />
+				<section className="section-btn" />
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default FindFalcone;
